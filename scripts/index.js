6f98d3791e5ed9e2d0436e365485eba3973b0f22
bedrein <dimabedrein118@gmail.com>
2023-01-22 17:42:48 +0300

    main_rev_2_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,36 @@
-
 let page = document.querySelector('.page');
 let profile = page.querySelector('.profile');
 let popup = page.querySelector('.popup');
 let elements = page.querySelector('.elements');
 let editBbtn = profile.querySelector('.profile__edit-btn');
-let closeBtn = popup.querySelector('.popup__icon-close');
+let closeBtn = popup.querySelector('.popup__button-close');
 let formElement = popup.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-job');
+let nameInput = popup.querySelector('.popup__input_text_name');
+let jobInput = popup.querySelector('.popup__input_text_job');
 let name = profile.querySelector('.profile__name');
 let job  = profile.querySelector('.profile__job');
 
 //Открыть popup
-editBbtn.addEventListener('click', playPopup);
 function playPopup() {
 	popup.classList.add('popup_opened');
-}
-
-//Закрыть popup 
-closeBtn.addEventListener('click', closePopup);
-function closePopup() { 
-	popup.classList.remove('popup_opened');
 	nameInput.value = name.textContent; 
 	jobInput.value  = job.textContent;
 }
 
+//Закрыть popup 
+function closePopup() { 
+	popup.classList.remove('popup_opened');
+}
+
 // Работа с input
-formElement.addEventListener('submit', handleFormSubmit); 
 function handleFormSubmit (evt) {
   evt.preventDefault(); 
   name.textContent = nameInput.value;
 	job.textContent = jobInput.value;
-	popup.classList.remove('popup_opened');
-	closeBtn.addEventListener('click', closePopup);
+	closePopup();
 }
 
+editBbtn.addEventListener('click', playPopup);
+closeBtn.addEventListener('click', closePopup);
 
-
-
+formElement.addEventListener('submit', handleFormSubmit); 
